a0eeddf82f5e169b064fbfc694800be5c8a1ae50
Rahul Jha <theslytherin@yandex.com>
2023-12-10 06:27:36 +0530

    added more embedding options

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import openai
 import os
 import pandas as pd
 from langchain.vectorstores import FAISS
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, OllamaEmbeddings, HuggingFaceEmbeddings
 from langchain.schema import Document
 
 from langchain.prompts import PromptTemplate
@@ -19,9 +19,11 @@ from langchain.prompts import PromptTemplate
 from langchain.schema import StrOutputParser
 from langchain.chat_models import ChatOpenAI
 import aux
+import yaml
+import typing
 st.title('Team 8️⃣0️⃣')
 
-
+model_map = yaml.load(open('model_map.yaml'), Loader=yaml.FullLoader)
 if 'api_key' not in st.session_state:
     st.session_state['api_key'] = ''
 if 'api_retriever' not in st.session_state:
@@ -33,14 +35,34 @@ if 'Ex_Ret' not in st.session_state:
 if 'API_Ret' not in st.session_state:
     st.session_state['API_Ret'] = None
 if 'URL' not in st.session_state:
-    st.session_state['URL'] = 'http://localhost:8000/chat'
+    st.session_state['URL'] = 'http://localhost:8000'
 
+
+
+#Util function
+def get_embedding_model(modelname : str, model_map : typing.Dict = model_map):
+    try:
+        model_type = model_map['embedding'][modelname].lower()
+        if model_type == 'openai':
+            if not st.session_state['api_key'].startswith('sk-'):
+                    st.warning(st.session_state['api_key'])
+                    st.warning('Please enter your OpenAI API key!', icon='⚠')
+            return OpenAIEmbeddings(model=modelname, api_key=st.session_state['api_key'])
+        elif model_type == 'ollama':
+            return OllamaEmbeddings(model=modelname)
+        elif model_type == 'sent':
+            return HuggingFaceEmbeddings(model_name=modelname)
+        else:
+            return KeyError(f"Model Type {model_type} not found")
+    except KeyError:
+        st.error("Model not found")
+        return None
 
 with st.expander("SETUP ⚙️", True):    
     with st.form('Data_Form'):
         url = st.text_input('URL', value=st.session_state['URL'], help='URL of the server where the API is hosted')
-
         text = st.text_input('API Key', type='password', help='Need it for proprietary model/retriever', value="LOL")
+        embedding_model = st.selectbox("Embedding Model", ['text-embedding-ada-002', 'llama2', 'all-MiniLM-L6-v2'])
         api_docs = st.file_uploader("Upload API Docs", type=['json'])
         if api_docs:
             api_docs = json.load(api_docs)
@@ -50,11 +72,14 @@ with st.expander("SETUP ⚙️", True):
 
         submitted = st.form_submit_button('Submit')
         if submitted:
+            
             st.session_state['URL'] = url
             st.session_state['api_key'] = text
-            if not st.session_state['api_key'].startswith('sk-'):
-                st.warning(st.session_state['api_key'])
-                st.warning('Please enter your OpenAI API key!', icon='⚠')
+            
+            with st.spinner("Loading Embedding Model..."):
+                st.session_state['embedding_model'] = get_embedding_model(embedding_model)
+
+            print(st.session_state['embedding_model'])
 
             with st.spinner("Loading API Descriptions..."):
                 API_descriptions = [
@@ -62,27 +87,21 @@ with st.expander("SETUP ⚙️", True):
                 for i, t in enumerate(api_docs)
                 ]
                 API_descriptions_vector_store = FAISS.from_documents(
-                    API_descriptions, OpenAIEmbeddings(api_key=st.session_state['api_key']))
+                    API_descriptions, st.session_state['embedding_model'])
                 API_Retriever = API_descriptions_vector_store.as_retriever()
                 st.session_state['api_retriever'] = True
             with st.spinner("Loading Examples.."):
                 API_usage_examples = [
-                
                     Document(page_content=t['Query'], metadata={"index": i})
                 for i, t in enumerate(api_examples)
                 ]
                 API_usage_examples_vector_score = FAISS.from_documents(
-                    API_usage_examples, OpenAIEmbeddings(api_key=st.session_state['api_key']))
+                    API_usage_examples, st.session_state['embedding_model'])
                 
                 Examples_Retriever = API_usage_examples_vector_score.as_retriever()
                 st.session_state['example_retriever'] = True
                 st.session_state['Ex_Ret'] = Examples_Retriever
                 st.session_state['API_Ret'] = API_Retriever
-    if not st.session_state['api_key'].startswith('sk'):
-        st.error("Invalid OpenAI Key")
-    else:
-        st.success("Valid Key Found!!")
-
     if not st.session_state['api_retriever']:
         st.error("Please upload API Descriptions")
     else:
@@ -96,7 +115,7 @@ with st.expander("SETUP ⚙️", True):
 
 
 with st.container(border=True):
-    model_id = st.selectbox("Choose Model", ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo', 'llama2'])
+    model_id = st.selectbox("Choose Model", ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo', 'llama2', 'orca-mini', 'wizard-vicuna-uncensored', 'llama2-uncensored'])
         
     use_piro = st.toggle("Get JSON", value=True, help="Toggle this if youo want JSON for your query and don't want to chat :smile:")
 
@@ -110,7 +129,7 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})    
-    if not (st.session_state['api_key'].startswith('sk-') and st.session_state['api_retriever'] and st.session_state['example_retriever']):
+    if not (st.session_state['api_retriever'] and st.session_state['example_retriever']):
         st.error("Invalid params, please recheck Setup")
     else:
         with st.spinner("Sending Request..."):
