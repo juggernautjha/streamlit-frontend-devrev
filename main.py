ece30335c97992cb45ebe7c2cc484972e0278bf9
Rahul Jha <theslytherin@yandex.com>
2023-12-11 22:27:56 +0530

    gradio sucks harder than a new dyson

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ import yaml
 import typing
 st.title('Team 8️⃣0️⃣')
 
+
+
 model_map = yaml.load(open('model_map.yaml'), Loader=yaml.FullLoader)
 if 'api_key' not in st.session_state:
     st.session_state['api_key'] = ''
@@ -64,11 +66,15 @@ with st.expander("SETUP ⚙️", True):
         text = st.text_input('API Key', type='password', help='Need it for proprietary model/retriever', value="LOL")
         embedding_model = st.selectbox("Embedding Model", ['text-embedding-ada-002', 'llama2', 'all-MiniLM-L6-v2'])
         api_docs = st.file_uploader("Upload API Docs", type=['json'])
+        api_examples = st.file_uploader("Upload API Examples", type=['json'])
         if api_docs:
             api_docs = json.load(api_docs)
-        api_examples = st.file_uploader("Upload API Examples", type=['json'])
         if api_examples:
             api_examples = json.load(api_examples)
+        if not api_docs and not api_examples:
+            st.warning("Please upload API Docs and Examples if you want to use ToolMode")
+            api_docs = []
+            api_examples = []
 
         submitted = st.form_submit_button('Submit')
         if submitted:
@@ -79,36 +85,43 @@ with st.expander("SETUP ⚙️", True):
             with st.spinner("Loading Embedding Model..."):
                 st.session_state['embedding_model'] = get_embedding_model(embedding_model)
 
-            print(st.session_state['embedding_model'])
 
-            with st.spinner("Loading API Descriptions..."):
-                API_descriptions = [
-                Document(page_content=t['description'], metadata={"index": i})
-                for i, t in enumerate(api_docs)
-                ]
-                API_descriptions_vector_store = FAISS.from_documents(
-                    API_descriptions, st.session_state['embedding_model'])
-                API_Retriever = API_descriptions_vector_store.as_retriever()
-                st.session_state['api_retriever'] = True
-            with st.spinner("Loading Examples.."):
-                API_usage_examples = [
-                    Document(page_content=t['Query'], metadata={"index": i})
-                for i, t in enumerate(api_examples)
-                ]
-                API_usage_examples_vector_score = FAISS.from_documents(
-                    API_usage_examples, st.session_state['embedding_model'])
-                
-                Examples_Retriever = API_usage_examples_vector_score.as_retriever()
-                st.session_state['example_retriever'] = True
-                st.session_state['Ex_Ret'] = Examples_Retriever
-                st.session_state['API_Ret'] = API_Retriever
+            if len(api_docs) > 0 and len(api_examples) > 0:
+
+                with st.spinner("Loading API Descriptions..."):
+                    API_descriptions = [
+                    Document(page_content=t['description'], metadata={"index": i})
+                    for i, t in enumerate(api_docs)
+                    ]
+                    API_descriptions_vector_store = FAISS.from_documents(
+                        API_descriptions, st.session_state['embedding_model'])
+                    API_Retriever = API_descriptions_vector_store.as_retriever()
+                    st.session_state['api_retriever'] = True
+                with st.spinner("Loading Examples.."):
+                    API_usage_examples = [
+                        Document(page_content=t['Query'], metadata={"index": i})
+                    for i, t in enumerate(api_examples)
+                    ]
+                    API_usage_examples_vector_score = FAISS.from_documents(
+                        API_usage_examples, st.session_state['embedding_model'])
+                    
+                    Examples_Retriever = API_usage_examples_vector_score.as_retriever()
+                    st.session_state['example_retriever'] = True
+                    st.session_state['Ex_Ret'] = Examples_Retriever
+                    st.session_state['API_Ret'] = API_Retriever
+            else:
+                st.session_state['api_retriever'] = False
+                st.session_state['example_retriever'] = False
+                st.session_state['Ex_Ret'] = None
+                st.session_state['API_Ret'] = None
+
     if not st.session_state['api_retriever']:
-        st.error("Please upload API Descriptions")
+        st.error("No API Descriptions, Can't use ToolMode")
     else:
         st.success("API Description Loaded ✔")
 
     if not st.session_state['example_retriever']:
-        st.error("Please upload Examples")
+        st.error("No Usage Examples, Can't use ToolMode")
     else:
         st.success("Examples Loaded ✔")
 
@@ -117,42 +130,34 @@ with st.expander("SETUP ⚙️", True):
 with st.container(border=True):
     model_id = st.selectbox("Choose Model", ['gpt-3.5-turbo', 'gpt-4', 'gpt-4-turbo', 'llama2', 'orca-mini', 'wizard-vicuna-uncensored', 'llama2-uncensored'])
         
-    use_piro = st.toggle("Get JSON", value=True, help="Toggle this if youo want JSON for your query and don't want to chat :smile:")
+    use_piro = st.toggle("ToolMode", value=False, help="Toggle this if you want your ToolUse queries answered")
 
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+
 
 if prompt := st.chat_input("What is up?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})    
-    if not (st.session_state['api_retriever'] and st.session_state['example_retriever']):
-        st.error("Invalid params, please recheck Setup")
-    else:
-        with st.spinner("Sending Request..."):
-            response = aux.post_request(
-                st.session_state['URL'],prompt, api_docs, api_examples, st.session_state['api_key'], model_id, use_piro, st.session_state['API_Ret'], st.session_state['Ex_Ret']
-            )
+    with st.spinner("Sending Request..."):
+        response = aux.post_request(
+            st.session_state['URL'],prompt, api_docs, api_examples, st.session_state['api_key'], model_id, use_piro, st.session_state['API_Ret'], st.session_state['Ex_Ret']
+        )
         
 
     if not use_piro:
         with st.chat_message("human"):
-            st.markdown(prompt)
+            st.session_state.messages.append({"role": "human", "content": prompt})
         with st.chat_message("ai"):
-            st.markdown(response['output']['content'])
             st.session_state.messages.append({"role": "ai", "content": response['output']['content']})
     else:
         for i, j in response['output'][1]:
             with st.chat_message("human"):
-                st.markdown(i)
+                st.session_state.messages.append({"role": "human", "content": i})
             with st.chat_message("ai"):
                 st.session_state.messages.append({"role": "ai", "content": j})
 
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.text(message["content"])
 
-
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
